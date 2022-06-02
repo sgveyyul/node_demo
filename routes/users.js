9cd091223f9815d0950ac24b99f937b09b4276ec
Yul Stewart Gurrea <Yul.Stewart.Gurrea@ph.ey.com>
2022-06-02 22:10:02 +0800

    added user crud

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,9 @@ const userController = require('../controllers/users')
 const router = express.Router();
 
 router.get('', userController.getUsers);
+router.get('/:user_id', userController.getUser);
+router.post('', userController.createUsers);
+router.put('/:user_id', userController.updateUser);
+router.delete('/:user_id', userController.deleteUser);
 
 module.exports = router;
